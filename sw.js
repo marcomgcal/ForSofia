70a34167234a0c3b04ae2b894b5b1beb4cee1afe
marcomgcal <73779533+marcomgcal@users.noreply.github.com>
2026-09-18 21:52:37 +0800

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,69 +1,42 @@
-const CACHE_NAME="for-sofia-v1";
+const CACHE_NAME = "for-sofia-v1";
 
-const urlsToCache=[
-"/",
-"/index.html",
-"/style.css",
-"/script.js",
-"/manifest.json",
-"/images/photo1.jpg",
-"/images/photo2.jpg",
-"/images/photo3.jpg",
-"/images/photo4.jpg"
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+
+  "/041569D8-1E29-4ADB-8D2C-907D2EEA402F.png",
+  "/3E532DB9-1744-4FA3-B695-7530707F9E5F.jpeg",
+  "/645BDD27-2ACF-427F-8FB8-E530815554EF.jpeg",
+  "/D96AAB3F-820E-4A5B-94A3-75F0CD6899A4.jpeg"
 ];
 
-self.addEventListener("install",event=>{
-
-event.waitUntil(
-
-caches.open(CACHE_NAME)
-
-.then(cache=>cache.addAll(urlsToCache))
-
-);
-
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch",event=>{
-
-event.respondWith(
-
-caches.match(event.request)
-
-.then(response=>{
-
-return response||fetch(event.request);
-
-})
-
-);
-
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
 
-self.addEventListener("activate",event=>{
-
-event.waitUntil(
-
-caches.keys()
-
-.then(keys=>{
-
-return Promise.all(
-
-keys.map(key=>{
-
-if(key!==CACHE_NAME){
-
-return caches.delete(key);
-
-}
-
-})
-
-);
-
-})
-
-);
-
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
 });
